ba8c9c8162163d1d470e39da04ca885cfc749000
Raphael Radespiel <contatoraphael1701@gmail.com>
2023-03-10 11:30:52 -0300

    Create Products Schema and populate it with mock data

--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -3,10 +3,12 @@ import "../assets/Navbar.css"
 function Navbar(){
   return(
     <nav>
-      <h1>Circuit Cart</h1>
-      <select></select>
-      <input type="text"></input>
-      <button>Search</button>
+      <h1>C i r c u i t C a r t</h1>
+      <div className="filtered-search">
+        <select></select>
+        <input type="text"></input>
+        <button>Search</button>
+      </div>
       {/* 
         USER SETTINGS SECTION
         JUST A LOG IN BUTTON IF NOT LOGGED IN
