fffa529cce8521b3714f58b4c5c0b7a3d2348f0a
Raphael Radespiel <contatoraphael1701@gmail.com>
2023-03-10 20:56:48 -0300

    Integrate mysql database with node server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,19 @@
 const express = require("express");
+const app = express();
+
 const path = require("path");
 
-const app = express();
+const dotenv = require("dotenv").config().parsed;
+
+const mysql = require("mysql");
+const connection = mysql.createConnection({
+  host: dotenv.DB_HOST,
+  user: dotenv.DB_USER,
+  password: dotenv.DB_PASSWORD,
+  database: dotenv.DB_NAME
+});
+
+connection.connect();
 
 app.use("/", express.static(path.join(__dirname, "../client")));
 
@@ -12,8 +24,12 @@ app.get("/*", (_req, res) => {
 const { PORT = 5000 } = process.env;
 
 app.listen(PORT, () => {
-  console.log();
-  console.log(`  App running in port ${PORT}`);
-  console.log();
+  console.log(`  App running in port ${PORT}\n`);
   console.log(`  > Local: \x1b[36mhttp://localhost:\x1b[1m${PORT}/\x1b[0m`); 
+});
+
+process.on('exit', () => {
+  console.log('About to close');
+  // THROW ERRORS IN FUNCTION
+  connection.end(); 
 });
