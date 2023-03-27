7e3483d61b7b91b2e76c74436de362b9387f8b5f
Raphael Radespiel <contatoraphael1701@gmail.com>
2023-03-27 11:22:02 -0300

    Add correct images and n new sql product entries to begin working on Carousel

--- client/components/Hamburger.jsx
+++ client/components/Hamburger.jsx
@@ -26,7 +26,7 @@ function Hamburger(props){
           (<svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" viewBox="0 0 24 24"><path d="m16.192 6.344-4.243 4.242-4.242-4.242-1.414 1.414L10.535 12l-4.242 4.242 1.414 1.414 4.242-4.242 4.243 4.242 1.414-1.414L13.364 12l4.242-4.242z"/></svg>)}
         </button>
         {isDropDown && (
-        <div className="hamburger-dropdown">
+        <div className="hamburger-dropdown" onClick={updateDropDownState}>
           <Link className="react-router-links" to="account">Account</Link>
           <div className="middle-divisor"></div>
           <Link className="react-router-links" to="shopping-cart">Shopping Cart</Link>
