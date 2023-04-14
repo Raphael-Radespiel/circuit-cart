bbef317c8d34e91349c8626355aa36d0a404b784
Raphael Radespiel <contatoraphael1701@gmail.com>
2023-04-14 10:41:51 -0300

    Add minor improvements to the Searchbar component
    - use strict comparison
    - use `` string when calculating select width to simplify the code
    - Remove button from search bar button and keep it as React Router
    Link as to help with accessibility (tab was selecting the link then button
    which is redundant and anoying)
    - Fix sizing problems by adding a min-width

--- client/components/Searchbar.jsx
+++ client/components/Searchbar.jsx
@@ -1,19 +1,22 @@
 import { Link } from "react-router-dom"
 import {useState} from "react";
+
+import SearchIcon from "../assets/icons/SearchIcon"
+
 import "../assets/css/Searchbar.css";
 
 function Searchbar(){
   const [selectWidth, setSelectWidth] = useState({width: "48px"});
   const [searchColor, setSearchColor] = useState({color: "#F2E7D335"});
 
+  // Calculate an approximate width for select box based on value selected
   function updateSelectWidth(e){
-    // Calculate new select width based on string size
-    let newSelectWidth = e.target.value.length * 10 + 18;
-    setSelectWidth({width: (newSelectWidth + "px")});
+    let newSelectWidth = `${e.target.value.length * 10 + 18}px`;
+    setSelectWidth({width: newSelectWidth});
   }
 
   function updateSearchColor(e){
-    if(e.target.value == ""){
+    if(e.target.value === ""){
       setSearchColor({color: "#F2E7D335"});
     }
     else{
@@ -33,10 +36,8 @@ function Searchbar(){
         <option value="kits">Kits</option> 
       </select>
       <input type="text" style={searchColor} placeholder="Search for Components" onChange={e => updateSearchColor(e)}></input>
-      <Link className="react-router-links" to="/search">
-        <button className="search-button">
-          <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" viewBox="0 0 24 24"><path d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z"/></svg>
-        </button>
+      <Link className="react-router-links search-button" to="/search">
+        <SearchIcon width="27" height="27"/>
       </Link>
     </div>
   )
