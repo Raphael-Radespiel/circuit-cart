adc824553463c44b3ae4efda87069c77064a9554
Raphael Radespiel <contatoraphael1701@gmail.com>
2023-04-18 13:42:25 -0300

    Add basic displaying off returned search results

--- client/pages/search/Search.jsx
+++ client/pages/search/Search.jsx
@@ -1,7 +1,45 @@
+import { useState, useEffect } from "react";
+
 function Search(){
+  const [searchResults, setSearchResults] = useState([]);
+
+  // THIS WILL HAVE TO BE A UTIL FUNCTION BECAUSE VALIDATE.jsx USES IT
+  function getQueryParam(param) {
+    const rx = new RegExp("[?&]" + param + "=([^&]+).*$");
+    const returnVal = String(window.location).match(rx);
+    return returnVal === null ? "" : returnVal[1];
+  }
+
+  useEffect(() => {
+    async function fetchSearchResult(){
+      const filter = getQueryParam('filter');
+      const search = getQueryParam('search');
+
+      const url = `/search?filter=${filter}&search=${search}`;
+
+      const result = await fetch(url, {method: 'GET'});
+      const jsonResult = await result.json();
+
+      setSearchResults(jsonResult);
+    }
+
+    fetchSearchResult();
+  }, []);
+
   return (
     <div>
-      Search
+      {
+        searchResults.map((value) => {
+          return (
+            <div key={value.ProductID}>
+              <h2>{value.Title}</h2> 
+              <p>{value.Description}</p> 
+              <p>{value.Price}</p> 
+              <p>{value.AmountInStock}</p> 
+            </div>
+          )
+        })
+      }
     </div>
   )
 }
