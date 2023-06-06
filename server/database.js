79dffb3c34d16e14d87a8ebdfdc7360131d2c36f
Raphael Radespiel <contatoraphael1701@gmail.com>
2023-06-06 15:51:25 -0300

    Undo mistake of trying railway

--- server/database.js
+++ server/database.js
@@ -2,17 +2,12 @@ const dotenv = require("dotenv").config().parsed;
 
 const mysql = require("mysql");
 
-/*
-I AM NOW USING RAILWAY SO ILL JUST USE A CONNECTION STRING
 const connection = mysql.createConnection({
   host: dotenv.DB_HOST,
   user: dotenv.DB_USER,
   password: dotenv.DB_PASSWORD,
   database: dotenv.DB_NAME
 });
-*/
-
-const connection = mysql.createConnection(dotenv.DB_CONNECTION_STRING);
 
 connection.connect((err) => {
   try{
